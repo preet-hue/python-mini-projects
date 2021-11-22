14710a2417d8d9cafed75091ff2e5fdbe00f36bd
Preet-hue <Preetshokeen0001@gmail.com>
2021-11-22 13:13:55 +0530

    finished giving some more functionalities to the game.

--- guess-the-number-game.py
+++ guess-the-number-game.py
@@ -1,16 +1,34 @@
 import random
+random_number = random.randint(0, 1000) # This generates a random number between 0 and 1000
+chances_given = 10 # This is the count of chances given, if the attempts exceeds the number of chances the game gets over.
+attempts = 0 # These are the attempts made, after each attempt this number would be incremented by 1.
 
-random_number = random.randint(0, 1000)
-
+print("You have to guess a number between 0 and 1000.\nYou have 10 attempts.\nAfter every attempt, you get an "
+      "hint such as guess a smaller number.\nIf you guess the number correctly in 10 attempt, you win.\n\n Good Luck!!!")
 
 guessed_no = int(input("guess a number between 0 and 1000!!! -> "))
+attempts += 1
+print(f"You have {chances_given - attempts} attempts left")
+
 while True:
     if guessed_no == random_number:
         print("Congratulations, You Won!!!")
         break
+
+    elif attempts == chances_given:
+        print(f"You lost... The number was {random_number}")
+        break
+
     elif guessed_no > random_number:
         guessed_no = int(input("Guess a smaller number!!! -> "))
+        attempts += 1
+        print(f"You have {chances_given - attempts} attempts left")
         continue
+
     else:
         guessed_no = int(input("Guess a larger number!!! -> "))
+        attempts += 1
+        print(f"You have {chances_given - attempts} attempts left")
         continue
+
+input("Press enter to exit")
