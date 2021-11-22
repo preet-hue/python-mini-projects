925e5b876eb0559c8f928bab75b074acb339b25d
Preet-hue <Preetshokeen0001@gmail.com>
2021-11-22 13:19:43 +0530

    improved spacing

--- guess-the-number-game.py
+++ guess-the-number-game.py
@@ -8,27 +8,27 @@ print("You have to guess a number between 0 and 1000.\nYou have 10 attempts.\nAf
 
 guessed_no = int(input("guess a number between 0 and 1000!!! -> "))
 attempts += 1
-print(f"You have {chances_given - attempts} attempts left")
+print(f"\nYou have {chances_given - attempts} attempts left")
 
 while True:
     if guessed_no == random_number:
-        print("Congratulations, You Won!!!")
+        print("\nCongratulations, You Won!!!")
         break
 
     elif attempts == chances_given:
-        print(f"You lost... The number was {random_number}")
+        print(f"\nYou lost... The number was {random_number}!!! Better luck next time...")
         break
 
     elif guessed_no > random_number:
-        guessed_no = int(input("Guess a smaller number!!! -> "))
+        guessed_no = int(input("\nGuess a smaller number!!! -> "))
         attempts += 1
-        print(f"You have {chances_given - attempts} attempts left")
+        print(f"\nYou have {chances_given - attempts} attempts left")
         continue
 
     else:
-        guessed_no = int(input("Guess a larger number!!! -> "))
+        guessed_no = int(input("\nGuess a larger number!!! -> "))
         attempts += 1
-        print(f"You have {chances_given - attempts} attempts left")
+        print(f"\nYou have {chances_given - attempts} attempts left")
         continue
 
-input("Press enter to exit")
+input("\nPress enter to exit")
